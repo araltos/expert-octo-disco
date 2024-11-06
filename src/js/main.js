a9bec8ad0d924c2363731d289cf0f6331f3e9ee8
Santiago <leonardo.jara.saltos@gmail.com>
2024-11-06 09:51:18 -0700

    uupdate to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,26 @@
 import { getParkData } from "./parkService.mjs";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const parkData = getParkData();
+const parkData = getParkData();
 
-  const disclaimer = document.querySelector(".disclaimer a");
-  disclaimer.href = parkData.url;
-  disclaimer.innerHTML = parkData.fullName;
+const disclaimer = document.querySelector(".disclaimer");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
 
-  document.title = parkData.fullName;
+document.title = parkData.fullName;
 
-  const heroImage = document.querySelector(".hero-image");
-  heroImage.src = parkData.images[0].url;
+const heroImage = document.querySelector(".hero-image");
+heroImage.src = parkData.images[0].url;
 
-  function parkInfoTemplate(info) {
-    return `
-      <a href="/" class="hero-banner__title">${info.fullName}</a>
-      <p class="hero-banner__subtitle">
-        <span>${info.designation}</span>
-        <span>${info.states.join(", ")}</span>
-      </p>`;
-  }
+function parkInfoTemplate(info) {
+  const statesArray = info.states.split(",");
+  const statesText = statesArray.join(", ");
+  
+  return `
+    <a href="/" class="hero-banner__title">${info.fullName}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${statesText}</span>
+    </p>`;
+}
 
-  document.querySelector(".hero-info").innerHTML = parkInfoTemplate(parkData);
-});
+document.querySelector(".hero-info").innerHTML = parkInfoTemplate(parkData);
