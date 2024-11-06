f045d8e60a1dff2336f6dc479c25bd2d6a2a4ec1
Santiago <leonardo.jara.saltos@gmail.com>
2024-11-06 09:23:33 -0700

    update for js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const heroImage = document.querySelector(".hero-image");
+heroImage.src = parkData.images[0].url;
+
+function parkInfoTemplate(info) {
+    return `
+      <a href="/" class="hero-banner__title">${info.fullName}</a>
+      <p class="hero-banner__subtitle">
+        <span>${info.designation}</span>
+        <span>${info.states.join(", ")}</span>
+      </p>`;
+  }
+  document.querySelector(".hero-info").innerHTML = parkInfoTemplate(parkData);
+  
