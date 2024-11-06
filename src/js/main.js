2d0ce5387a983567520fb2e6e6f40dd205f5783b
Santiago <leonardo.jara.saltos@gmail.com>
2024-11-06 09:42:24 -0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,18 @@
 import { getParkData } from "./parkService.mjs";
 
-const parkData = getParkData();
+document.addEventListener("DOMContentLoaded", () => {
+  const parkData = getParkData();
 
-const disclaimer = document.querySelector(".disclaimer a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
+  const disclaimer = document.querySelector(".disclaimer a");
+  disclaimer.href = parkData.url;
+  disclaimer.innerHTML = parkData.fullName;
 
-document.title = parkData.fullName;
+  document.title = parkData.fullName;
 
-const heroImage = document.querySelector(".hero-image");
-heroImage.src = parkData.images[0].url;
+  const heroImage = document.querySelector(".hero-image");
+  heroImage.src = parkData.images[0].url;
 
-function parkInfoTemplate(info) {
+  function parkInfoTemplate(info) {
     return `
       <a href="/" class="hero-banner__title">${info.fullName}</a>
       <p class="hero-banner__subtitle">
@@ -19,5 +20,6 @@ function parkInfoTemplate(info) {
         <span>${info.states.join(", ")}</span>
       </p>`;
   }
+
   document.querySelector(".hero-info").innerHTML = parkInfoTemplate(parkData);
-  
+});
