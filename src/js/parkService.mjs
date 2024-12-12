e1c57c8b480fbafd417d822b2b5c630051f11fb4
Santiago <leonardo.jara.saltos@gmail.com>
2024-12-12 09:10:16 -0700

    update

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -219,7 +219,6 @@ async function getJson(url) {
 }
 
 export function getInfoLinks(data) {
-  // Why index + 2 below? no real reason. we don't want index 0 since that is the one we used for the banner...I decided to skip an image.
   const withUpdatedImages = parkInfoLinks.map((item, index) => {
     item.image = data[index + 2].url;
     return item;
@@ -240,4 +239,9 @@ export async function getParkAlerts(code) {
 export async function getParkVisitorCenters(code) {
   const parkData = await getJson(`visitorcenters?parkCode=${code}`);
   return parkData.data;
+}
+
+export async function getParkVisitorCenterDetails(id) {
+  const parkData = await getJson(`visitorcenters?id=${id}`);
+  return parkData.data[0];
 }
